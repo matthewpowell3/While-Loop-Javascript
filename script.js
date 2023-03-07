240dc4c209a127d4869c08f335aca4986235a47b
matthewpowell3 <106132405+matthewpowell3@users.noreply.github.com>
2023-03-06 23:59:55 -0500

    1 file modified

--- script.js
+++ script.js
@@ -1,29 +1,7 @@
 ///While Loop Javascript
 var x = 1;
 while (x <= 5) {
-  switch (x) {
-    case 1:
-      console.log('One');
-      break;
-
-    case 2:
-      console.log('Two');
-      break;
-
-    case 3:
-      console.log('Three');
-      break;
-
-    case 4:
-      console.log('Four');
-      break;
-
-    case 5:
-      console.log('Five');
-      break;
-  }
-
-  // document.write('<h3>' + x + '</h3>');
+  document.write('<h3>' + x + '</h3>');
   // console.log(x);
   x += 2; // x=x + 2 increment by two
 }
