f8a70ea13784f4c5b79a9353d8b3ff59f4c2ccb1
matthewpowell3 <106132405+matthewpowell3@users.noreply.github.com>
2023-03-07 00:09:48 -0500

    1 file modified

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 ///While Loop Javascript
 var x = 1;
-while (x <= 5) {
+while (x <= 50) {
   document.write('<h3>' + x + '</h3>');
   // without a increment/decrement it will go in to a infinite loop
   // console.log(x);
   x += 2; // x=x + 2 increment by two
 }
+
+// use while loop when it comes to user input
