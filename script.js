d8256ecd4362908204039e06b8369fd17c0eab5c
matthewpowell3 <106132405+matthewpowell3@users.noreply.github.com>
2023-03-07 00:02:12 -0500

    1 file modified

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 var x = 1;
 while (x <= 5) {
   document.write('<h3>' + x + '</h3>');
+  // without a increment/decrement it will go in to a infinite loop
   // console.log(x);
   x += 2; // x=x + 2 increment by two
 }
